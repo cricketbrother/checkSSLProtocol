23f5927288cb61b3d1e8600ddca2c84ac69bc796
cricketbrother <zhouwenchao19870210@gmail.com>
2023-08-24 09:19:49 +0800

    modify workflow

--- flag.go
+++ flag.go
@@ -4,7 +4,6 @@ import "flag"
 
 var (
 	version string
-	buildTime string
 )
 
 // 获取参数
@@ -12,19 +11,16 @@ func initFlag() (string, string, string, int) {
 	flag.Usage = func() {
 		println("A tool to check SSL/TLS protocol support")
 		println("Version:    " + version)
-		println("Build Time: " + buildTime)
 		println()
 		println("Usage: checkSSLProtocol2 [-d domain] [-p port] [-f file] [-m max threads]")
-		println()
 		println("Options:")
 		flag.PrintDefaults()
-		println()
 		println("Notice: if -d, -p and -f are specified at the same time, -d and -p will be ignored")
-		println()
-		println("Example 1: checkSSLProtocol2 -d www.baidu.com -p 443")
-		println("Example 2: checkSSLProtocol2 -d www.baidu.com")
-		println("Example 3: checkSSLProtocol2 -f sites.txt")
-		println("Example 4: checkSSLProtocol2 -f sites.txt -m 20")
+		println("Examples:")
+		println("\tcheckSSLProtocol2 -d www.baidu.com -p 443")
+		println("\tcheckSSLProtocol2 -d www.baidu.com")
+		println("\tcheckSSLProtocol2 -f sites.txt")
+		println("\tcheckSSLProtocol2 -f sites.txt -m 20")
 	}
 	domain := flag.String("d", "", "domain")
 	port := flag.String("p", "443", "port")

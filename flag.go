3343cd1e022a92dc84daffe03a8dd08538cd551e
cricketbrother <zhouwenchao19870210@gmail.com>
2023-08-24 08:39:24 +0800

    add workflow

--- flag.go
+++ flag.go
@@ -2,10 +2,18 @@ package main
 
 import "flag"
 
+var (
+	version string
+	buildTime string
+)
+
 // 获取参数
 func initFlag() (string, string, string, int) {
 	flag.Usage = func() {
 		println("A tool to check SSL/TLS protocol support")
+		println("Version:    " + version)
+		println("Build Time: " + buildTime)
+		println()
 		println("Usage: checkSSLProtocol2 [-d domain] [-p port] [-f file] [-m max threads]")
 		println()
 		println("Options:")
